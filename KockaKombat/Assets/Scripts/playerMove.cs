2104d4106e92f47407ce75fc2f60e98642b3a780
Tibcsi0325 <banhalmi.tibcsi@gmail.com>
2019-04-18 15:21:16 +0200

    ef

---
+++ KockaKombat/Assets/Scripts/playerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovew : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        rb.AddForce(new Vector2(move,0));
+    }
+}
