ec5f9d3b612cf4d9dc470110d6d53ee4f62f1078
Tibcsi0325 <banhalmi.tibcsi@gmail.com>
2019-04-18 17:55:46 +0200

    semmi b@zdmeg

---
+++ KockaKombat/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float playerhealth;
+    public float playerdamage;
+    public float playerdamagemultiplyerboost;
+
+    void UpgradeHealth(float count)
+    {
+        playerhealth += count;
+    }
+
+    void UpgradeDamage(float count)
+    {
+        playerdamage += count;
+    }
+    void Upgradeplayerdamagemultiplyerboost(float count)
+    {
+        playerdamagemultiplyerboost += count;
+    }
+
+}
